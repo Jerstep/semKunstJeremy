1387a2cdb4fa3968ba4125b7338dc1fecc7998b9
Jeremy <jeremy.koskamp@live.nl>
2020-06-17 10:40:06 +0200

    [+] Initial Push

---
+++ SemKunst 2/Assets/Scripts/IKFollowTarget.cs
@@ -0,0 +1,46 @@
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+
+//public class IKFollowTarget : MonoBehaviour
+//{
+//    public Transform target;
+//    public Vector3 offset;
+
+//    private Animator[ anim;
+//    public List<GameObject> transformObjects;
+
+//    //List of objects in which i look for the right bones and tranform their position
+
+//    // Need a list of bones that follow target
+//    // Each bone willbe cycled through to transform its position to the target
+//    // Within a set range the bones can move (EXTRA)
+
+//    private void Start()
+//    {
+//        anim = this.GetComponentsInChildren<Animator>();
+//    }
+
+//    private void LateUpdate()
+//    {
+//        for (int i = 0; i < bonesToTransform.Count; i++)
+//        {
+//            anim = bonesToTransform[i].GetComponentInParent<Animator>();
+//            if (anim.GetBoneTransform(HumanBodyBones.LeftShoulder) != null)
+//            {
+//                bonesToTransform[i] = anim.GetBoneTransform(HumanBodyBones.LeftShoulder);
+//            }
+//            else
+//            {
+//                bonesToTransform[i] = anim.GetBoneTransform(HumanBodyBones.RightShoulder);
+//            }
+//            bonesToTransform[i].LookAt(target.position);
+//            bonesToTransform[i].rotation *= Quaternion.Euler(offset);
+//        }
+
+
+
+
+
+//    }
+//}
---
+++ SemKunst 2/Assets/Scripts/IKFollowTarget1.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IKFollowTarget1 : MonoBehaviour
+{
+    public Transform target;
+    public Transform objectTransform;
+
+    public Vector3 offsetArm1;
+    public Vector3 offsetArm2;
+
+    private Animator anim;
+    public List<Transform> bonesToTransform;
+
+    private void Start()
+    {
+        objectTransform = this.transform;
+        anim = this.GetComponent<Animator>();
+    }
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < bonesToTransform.Count; i++)
+        {
+            bonesToTransform[i].LookAt(target.position);
+            bonesToTransform[0].rotation *= Quaternion.Euler(offsetArm1);
+            bonesToTransform[1].rotation *= Quaternion.Euler(offsetArm2);
+        }
+
+
+    }
+}
